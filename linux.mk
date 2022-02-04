2ab7842a4f4a5791baa96dac9295d46ad4d28997
Bert van der Weerd <bert@stanzabird.nl>
2022-02-04 22:43:00 +0100

    fixes

--- linux.mk
+++ linux.mk
@@ -7,13 +7,17 @@ version:=$(shell cat version)
 release:=$(shell cat release)
 source_release:=$(shell cat source_release)
 
+outfile=librewolf-$(version)-$(release).en-US.$(distro)-x86_64.tar.bz2
+
 docker :
 	docker build --build-arg "distro=$(distro_image)" -t librewolf/bsys5-image-$(distro) - < Dockerfile
 
 build : work
 	docker run --rm -v $(shell pwd)/work:/work:rw librewolf/bsys5-image-$(distro) sh -c "cd /work/librewolf-$(version) && ./mach build && ./mach package"
-	cp -v work/librewolf-$(version)/obj-x86_64-pc-linux-gnu/dist/librewolf-$(version)-$(source_release).en-US.linux-x86_64.tar.bz2 librewolf-$(version)-$(release).en-US.$(distro)-x86_64.tar.bz2 
+	cp -v work/librewolf-$(version)/obj-x86_64-pc-linux-gnu/dist/librewolf-$(version)-$(source_release).en-US.linux-x86_64.tar.bz2 $(outfile)
+	sha256sum $(outfile)
 
 ci : work
 	(cd /work/librewolf-$(version) && ./mach build && ./mach package)
-	cp -v work/librewolf-$(version)/obj-x86_64-pc-linux-gnu/dist/librewolf-$(version)-$(source_release).en-US.linux-x86_64.tar.bz2 librewolf-$(version)-$(release).en-US.$(distro)-x86_64.tar.bz2 
+	cp -v work/librewolf-$(version)/obj-x86_64-pc-linux-gnu/dist/librewolf-$(version)-$(source_release).en-US.linux-x86_64.tar.bz2 $(outfile)
+	sha256sum $(outfile)
