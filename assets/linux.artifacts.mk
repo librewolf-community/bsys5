619c576f794a00b4a5bfd9d33e63bc924fbe2f4b
Bert van der Weerd <bert@stanzabird.nl>
2022-02-11 07:10:10 +0100

    first version that produces deb+rpm

--- assets/linux.artifacts.mk
+++ assets/linux.artifacts.mk
@@ -11,18 +11,17 @@ use_docker=true
 
 infile=librewolf-$(version)-$(release).en-US.$(distro)-x86_64.tar.bz2
 
-
-###### .DEB ######
-
-
+#
+# Debian based:
+#
 
 librewolf-$(version)-$(release).en-US.$(distro).x86_64.deb : $(infile)
+	@echo ""
 	@echo "[debug] Building DEB:" $@ "using as source:" $<
-
+	@echo ""
 	mkdir -p work
 	(cd work && tar xf ../$<)
 	cp -v assets/linux.build-deb.sh work/
-	sed "s/MYDIR/\/usr\/share\/librewolf/g" < assets/linux.librewolf.desktop.in > work/start-librewolf.desktop
 	if [ $(use_docker) = true ]; then \
 		docker run --rm -v $(shell pwd)/work:/work:rw librewolf/bsys5-image-$(distro) sh -c "bash linux.build-deb.sh $(version) $(release)" ; \
 	else \
@@ -32,29 +31,40 @@ librewolf-$(version)-$(release).en-US.$(distro).x86_64.deb : $(infile)
 	sha256sum $@ > $@.sha256sum
 	cat $@.sha256sum
 
-
-
 artifacts-deb : $(infile) $(infile).sha256sum
 	sha256sum -c $(infile).sha256sum
 	${MAKE} -f assets/linux.artifacts.mk distro=$(distro) librewolf-$(version)-$(release).en-US.$(distro).x86_64.deb
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-###### .RPM ######
+#
+# RPM Based:
+#
 
 librewolf-$(version)-$(release).$(fc).x86_64.rpm : $(infile)
+	@echo ""
 	@echo "[debug] Building RPM:" $@ "using as source:" $<
+	@echo ""
+	mkdir -p work
+	(cd work && tar xf ../$<)
+	cp -v assets/linux.build-rpm.sh work
+	cp -v version work
+	cp -v release work
+	cp -v assets/linux.librewolf.spec work/librewolf.spec
+	cp -v assets/linux.librewolf.desktop.in work/librewolf/start-librewolf.desktop.in
+	cp -v assets/linux.librewolf.ico work/librewolf/librewolf.ico
+	rm -f work/librewolf/browser/features/proxy-failover@mozilla.com.xpi
+	rm -f work/librewolf/pingsender
+	rm -f work/librewolf/precomplete
+	rm -f work/librewolf/removed-files
+	if [ $(use_docker) = true ]; then \
+		docker run --rm -v $(shell pwd)/work:/work:rw librewolf/bsys5-image-$(distro) sh -c "bash linux.build-rpm.sh $(version) $(release)" ; \
+	else \
+		(cd / && cp -r /work / && cd /work && bash linux.build-rpm.sh $(version) $(release)) ; \
+	fi
+	cp -v work/$@ $@
+	sha256sum $@ > $@.sha256sum
+	cat $@.sha256sum
 
 artifacts-rpm : $(infile) $(infile).sha256sum
 	sha256sum -c $(infile).sha256sum
